d7d4fb36f22f528e6b81c81151feb2178140da30
Jax-ai <jacques@mycloudwebsite.co.za>
2025-07-20 19:56:48 +0200

    remove element

---
+++ Top Interview 150/remove-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    
+    let write = 0;
+    
+    for (let read = 0; read < nums.length; read++) {
+        if (nums[read] !== val) {
+            nums[write] = nums[read];
+            write++;
+        }
+    }
+    
+    return write; // Number of elements not equal to val
+};
